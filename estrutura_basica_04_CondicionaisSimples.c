fd5c4835b27fb65002ea468d5e520359a7720054
Rodrigo Lima <62031871+zrodrigolimaz@users.noreply.github.com>
2021-09-04 22:16:16 -0300

    Add files via upload
    Passo 01 - Introdução a condicionais
    Condicional Simples ;

---
+++ estrutura_basica_04_CondicionaisSimples.c
@@ -0,0 +1,21 @@
+// CONDICIONAIS 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//função principal do programa
+
+int main () {
+	// Definindo variáveis. 
+	int a = 4;
+	
+	// Condicional simples        (=) -> ATRIBUIÇÃO  (==) -> COMPARAÇÃO
+	if (a == 4){
+		printf("\n A Variavel a = 4");
+	}else {
+		printf (" A variavel  não é igual a 4");
+	}
+
+	return 0;
+	
+}
