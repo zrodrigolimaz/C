afbf4e67182fdf1a9a9bd8047b3b7dae811e3a17
Rodrigo Lima <62031871+zrodrigolimaz@users.noreply.github.com>
2021-09-04 22:04:14 -0300

    Add files via upload
    Operações de Adição, Subtração, Multiplicação, Divisão e resto.

---
+++ estrutura_basica_03_operadoresAritmeticos.c
@@ -0,0 +1,32 @@
+// OPERADORES MATEMATICOS 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//Função principal do programa
+
+int main () {
+	// definindo variáveis
+	int a = 6, b = 4;
+	
+	// Soma 
+	  	printf ("\n a soma de %d e %d = %d\n",a,b,a + b);
+	
+	// Subtração 
+		printf("\n a subtracao de %d e %d = %d\n",a, b, a - b);
+		
+	//	Multiplicação
+		printf("\n a multiplicacao de %d e %d = %d\n",a, b, a * b);
+		
+	// Divisão
+		printf("\n a Divisao de %d e %d = %d\n",a, b, a / b);
+		
+	// Resto da Divisão 
+		printf("\n O resto da divisao de %d e %d = %d\n",a, b, a % b);			
+		
+
+	
+	//pausa o programa após executar 
+	system ("pause");	
+	
+}
